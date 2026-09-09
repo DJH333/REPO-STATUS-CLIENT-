71fb2a5a527506ab6efe4ad1d3ad4edd25bf4967
DJH333 <dhenry64753@gmail.com>
2026-09-09 17:56:42 -0400

    Complete error handling: add rate limiting (429) and malformed JSON handling

--- main.py
+++ main.py
@@ -9,19 +9,26 @@ import sys
 load_dotenv()
 token = os.getenv("GITHUB_TOKEN")
 
-
 def get_github_response(username):
     try:
-        response = requests.get(f'https://api.github.com/users/{username}/repos' ,headers={"Authorization": f"Bearer {token}"}, timeout=10)
+        response = requests.get(f'https://api.github.com/users/{username}/repos', headers={"Authorization": f"Bearer {token}"}, timeout=10)
     except requests.exceptions.Timeout as err:
         print(f"Request timed out while trying to reach GitHub for username '{username}'")
         print(err)
         sys.exit(1)
-    if response.status_code != 200:
+    if response.status_code == 429:
+        print(f"Error: received status code {response.status_code}. You've hit GitHub's rate limit, try again later")
+        sys.exit(1)
+    elif response.status_code != 200:
         print(f"Error: received status code {response.status_code} for username '{username}'")
         sys.exit(1)
     else:
-        data = response.json()
+        try:
+            data = response.json()
+        except requests.exceptions.JSONDecodeError as err:
+            print(f"Error: received JSON format error for username '{username}'")
+            print(err)
+            sys.exit(1)
     return data
 
 def parse_repos(data):
